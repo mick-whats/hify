eec822612ec754fa774ef3c84c725c16501926ea
whats-mick <9634573+mick-whats@users.noreply.github.com>
2018-12-21 19:34:49 +0900

    fix

--- src/__tests__/strfy-attributes.test.js
+++ src/__tests__/strfy-attributes.test.js
@@ -1,11 +1,19 @@
 const fn = require('../strfy-attributes')
 
-test('should ', () => {
+test('example ', () => {
   expect(
     fn({
       id: 'myId',
-      class: 'myClass',
-      style: { color: 'red' }
+      class: ['myClass', 'u-20'],
+      isHappy: true,
+      style: { color: 'red', 'font-size': '14px' },
+      alt: ''
     })
-  ).toBe(' id="myId" class="myClass" style="color:red;"')
+  ).toBe(
+    ' id="myId" class="myClass u-20" isHappy style="color:red;font-size:14px;" alt=""'
+  )
+})
+
+test('nothing', () => {
+  expect(fn({})).toBe('')
 })
--- src/strfy-attributes.js
+++ src/strfy-attributes.js
@@ -1,39 +1,24 @@
-// convertAttributes = (attr)->
-//   str = ''
-//   if _.isPlainObject(attr)
-//     for key, value of attr
-//       if _.isPlainObject(value)
-//         str+= " #{key}=" + '"'
-//         for sKey, sVal of value
-//           str+= "#{sKey}:#{sVal};"
-//         str+= '"'
-//       else
-//         str+= " #{key}=" + '"' + value + '"'
-//   else if _.isString(attr)
-//     str = ' ' + attr
-//   return str
-
-function strfyAttributes(obj) {
+function strfyAttributes (obj) {
   const attributes = []
-  Object.entries(obj).forEach(([k,v])=> {
-    
-    if (typeof v === "function" || typeof v === "object" && !!v){
-      v = Object.entries(v).map(([ik,iv]) => {
-        return `${ik}:${iv};`
-      })
-      // v = `${key}="${valString}"`
-    } else if (v === false){
-      continue
-    } else if (Array.isArray(v)) {
-			v = v.join(' ');
-    } 
-    let attribute = key
+  Object.entries(obj).forEach(([k, v]) => {
+    if (Array.isArray(v)) {
+      v = v.join(' ')
+    } else if (typeof v === 'object' && !!v) {
+      v = Object.entries(v)
+        .map(([ik, iv]) => {
+          return `${ik}:${iv};`
+        })
+        .join('')
+    } else if (v === false) {
+      return
+    }
+    let attribute = k
     if (v !== true) {
-      let attribute = `=${String(v)}`
+      attribute += `="${String(v)}"`
     }
     attributes.push(attribute)
   })
-  return attributes.length > 0 ? ' ' + attributes.join(' ') : '';
+  return attributes.length > 0 ? ' ' + attributes.join(' ') : ''
 }
 
 module.exports = strfyAttributes
