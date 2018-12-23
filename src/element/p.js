dfa7be82a53bea1a1eb0c22fc27400eea5376a35
whats-mick <9634573+mick-whats@users.noreply.github.com>
2018-12-23 11:23:58 +0900

    削除予定

--- src/element/p.js
+++ src/element/p.js
@@ -1,19 +1,19 @@
-const CE = require('../createElement')
-const colorAttributes = require('../helper/color-attributes')
-/**
- * p (paragraph tag)
- *
- * @param {string|Array} cont - content
- * @param {*} [args] - colorAttributes
- * @returns {createElement}
- * @example
- * el.p('paragraph', 'red', 'gray', { id: 'Myid' }).render()
- *  // -> '<p id="Myid" style="color:red;background-color:gray;">paragraph</p>'
- *
- */
-function p (cont, ...args) {
-  const attr = colorAttributes(...args)
-  return new CE('p', attr, cont)
-}
+// const CE = require('../createElement')
+// const colorAttributes = require('../helper/color-attributes')
+// /**
+//  * p (paragraph tag)
+//  *
+//  * @param {string|Array} cont - content
+//  * @param {*} [args] - colorAttributes
+//  * @returns {createElement}
+//  * @example
+//  * el.p('paragraph', 'red', 'gray', { id: 'Myid' }).render()
+//  *  // -> '<p id="Myid" style="color:red;background-color:gray;">paragraph</p>'
+//  *
+//  */
+// function p (cont, ...args) {
+//   const attr = colorAttributes(...args)
+//   return new CE('p', attr, cont)
+// }
 
-module.exports = p
+// module.exports = p
