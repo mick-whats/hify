b1049d43d8330a817296ca75f59aae0537939b86
whats-mick <9634573+mick-whats@users.noreply.github.com>
2018-12-25 20:11:12 +0900

    rename

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,7 @@
 const os = require('os')
 const path = require('path')
 
-const PROJECT_NAME = 'xqx'
+const PROJECT_NAME = 'hify'
 const BASENAME = `node-${PROJECT_NAME}.html`
 
 module.exports.PROJECT_NAME = PROJECT_NAME
