bf0e0e43edb7e44b06f6eb18af6ce92fb47158ce
whats-mick <9634573+mick-whats@users.noreply.github.com>
2019-01-13 08:22:38 +0900

    docs部分をhify.docに切り分け

--- docSrc/container/navbarContainer.js
+++
@@ -1,8 +0,0 @@
-const { el, ex, uk } = require('../..')
-
-const main = el.div([el.h1('sidebarContainer'), el.p('hogemoge')])
-module.exports = uk.container({
-  main,
-  header: ex.ul(['foo', 'bar', 'baz']),
-  title: 'sidebarContainer sample'
-})
--- docSrc/container/sidebarContainer.js
+++
@@ -1,8 +0,0 @@
-const { el, ex, uk } = require('../..')
-
-const main = el.div([el.h1('sidebarContainer'), el.p('hogemoge')])
-module.exports = uk.sidebarContainer({
-  main,
-  side: ex.ul(['foo', 'bar', 'baz']),
-  title: 'sidebarContainer sample'
-})
